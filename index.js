9614b9aad8bc6a50f24e46264215be1fd54f88dc
Amar Hafizuddeen <amarhafizuddeen@gmail.com>
2019-02-15 10:26:23 +0800

    First commit.

---
+++ index.js
@@ -0,0 +1,63 @@
+/*
+ * Primary file for the API
+ *
+ */
+
+//  Dependencies
+const http = require("http");
+const url = require("url");
+const StringDecoder = require("string_decoder").StringDecoder;
+
+// The server should respond to all requests with a string
+const server = http.createServer((req, res) => {
+  // Get the URL and parse it
+  const parsedUrl = url.parse(req.url, true);
+
+  // Get the path from the URL
+  const path = parsedUrl.pathname;
+  const trimmedPath = path.replace(/^\/+|\/+$/g, "");
+
+  // Get the query string as an object
+  const queryStringObject = parsedUrl.query;
+
+  // Get the HTTP Method
+  const method = req.method.toLowerCase();
+
+  // Get the headers as an object
+  const headers = req.headers;
+
+  // Get the payload, if any
+  const decoder = new StringDecoder("utf-8");
+  let buffer = "";
+  req.on("data", data => {
+    buffer += decoder.write(data);
+  });
+  req.on("end", () => {
+    buffer += decoder.end();
+
+    // Send the response
+    res.end("Hello world\n");
+
+    // Log the request path
+    console.log("Request received with this payload: ", buffer);
+  });
+});
+
+// Start the server, and have it listen on port 3000
+server.listen(3000, () => {
+  console.log("The server is listening on port 3000");
+});
+
+// Define the handlers
+const handlers = {};
+
+// Sample handler
+handlers.sample = (data, callback) => {};
+
+// Not found handler
+handlers.notFound = (data, callback) => {};
+
+// Define a request router
+const router = {
+  sample: handlers.sample
+};
