88a18649dd33329d230ac9e534ecfdd685a2a0a7
Amar Hafizuddeen <amarhafizuddeen@gmail.com>
2019-03-07 10:38:57 +0800

    Added test runner for API calls.

--- test/index.js
+++ test/index.js
@@ -1,40 +1,20 @@
 /*
+ * Test Runner
  *
- * Test runner
  */
 
-//  Dependencies
-const helpers = require("./../lib/helpers");
-const assert = require("assert");
+// Override the NODE_ENV variable
+process.env.NODE_ENV = "testing";
 
 // Application logic for the test runner
 _app = {};
 
 // Container for the tests
-_app.tests = {
-  unit: {}
-};
+_app.tests = {};
 
-// Assert that the getANumber function is returning a number
-_app.tests.unit["helpers.getANumber should return a number"] = done => {
-  const val = helpers.getANumber();
-  assert.equal(typeof val, "number");
-  done();
-};
-
-// Assert that the getANumber function is returning 1
-_app.tests.unit["helpers.getANumber should return 1"] = done => {
-  const val = helpers.getANumber();
-  assert.equal(val, 1);
-  done();
-};
-
-// Assert that the getANumber function is returning 2
-_app.tests.unit["helpers.getANumber should return 2"] = done => {
-  const val = helpers.getANumber();
-  assert.equal(val, 2);
-  done();
-};
+// Add on the unit tests
+_app.tests.unit = require("./unit");
+_app.tests.api = require("./api");
 
 // Count all the tests
 _app.countTests = () => {
@@ -128,6 +108,7 @@ _app.produceTestReport = (limit, successes, errors) => {
 
   console.log("");
   console.log("--------------END TEST REPORT--------------");
+  process.exit(0);
 };
 
 // Run the tests
